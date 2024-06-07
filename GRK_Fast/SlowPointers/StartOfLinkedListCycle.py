7e359074fc86944a755b0f2562808fb02ac658e0
sakethc4 <saketh.c4@gmail.com>
2024-06-07 19:35:19 -0400

    sol commit

--- GRK_Fast/SlowPointers/StartOfLinkedListCycle.py
+++ GRK_Fast/SlowPointers/StartOfLinkedListCycle.py
@@ -1,4 +1,4 @@
-class Solution:
+class Solution: # O(N) Time and O(1) Space 
     def findCycleStart(self, head): 
         # Given the head of a LL we want to find where the cycle starts. 
         # Sample input: 1 => 2=> 3 => 4 => 5 => 6=> 3 (cycle starts at 3)
@@ -6,15 +6,25 @@ class Solution:
         # Sol. We want a fast and slow pointer and we need to determine where the 
         # cycle starts the first sol that comes to mind is that we figure out where
         # the cycle starts if we figure out where the two pointers meet one another. 
-        # They should meet where the cycle starts, so we need to determine the length of 
-        # cycle and move the fast pointer by the length, while incrementing the slow one by 1. 
+        # They should meet where the cycle starts, so we need to determine the meeting point.
+        # After moving slow back to head and moving both at a constant rate. 
         # ----
-        # Initializing the two pointers 
-        slow, fast = head, head
-        # Variable to keep track of the length 
-        cycleLength = 0
-        while(fast is not None and fast.next is not None):
-            fast = fast.next.next
-            slow = slow.next
-            if slow == fast:
-                cycleLength = self.calculate
+        # Check to validate input head 
+        if head == None or head.next == None:
+            return None 
+        # Initialize our slow and fast moving pointers 
+        slow, fast = head, head 
+        # Iterate through input LL 
+        while fast is not None and fast.next is not None: 
+            # Moving the pointers as needed 
+            slow = slow.next 
+            fast = fast.next.next 
+            # If the two meet we have found the starting point of our cycle. 
+            if slow == fast:  
+                # We can set out slow pointer to head 
+                slow = head 
+                while slow != fast: 
+                    slow = slow.next 
+                    fast = fast.next 
+                return slow 
+        return None 
