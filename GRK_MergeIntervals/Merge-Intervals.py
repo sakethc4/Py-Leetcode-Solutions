e975b702d1096a1e65c57fc569fde2d893dadcae
sakethc4 <saketh.c4@gmail.com>
2024-06-19 14:04:31 -0400

    sol commit

---
+++ GRK_MergeIntervals/Merge-Intervals.py
@@ -0,0 +1,29 @@
+
+class Interval:
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+# O(N Log(N)) Time & O(N) Space. 
+class Solution: 
+    def merge(self, intervals):
+        # First we need to check if there are even enough intervals to merge.
+        if len(intervals) < 2:
+            return intervals 
+        # Look at intervals start time and sort them.
+        intervals.sort(key=lambda x: x.start)
+        # Initializing an empty list to do the merging. 
+        merged = [] 
+        start = intervals[0].start 
+        end = intervals[0].end
+        for index in range(1, len(intervals)):
+            interval = intervals[index]
+            if interval.srart <= end:
+                end = max(Interval.end, end)
+            else:
+                merged.append(Interval(start, end))
+                start = interval.start
+                end = interval.end 
+        # Need to account for the last interval. 
+                merged.append(Interval(start, end))
+                return merged
+            
--- GRK_SlidingWindow/FruitsIntoBaskets.py
+++ GRK_SlidingWindow/FruitsIntoBaskets.py
@@ -15,17 +15,17 @@ class Solution: # O(N) Time and O(K) Space
                 fruitFrequency[rightFruit] = 0 
             # Otherwise add an occurence.
             fruitFrequency[rightFruit] += 1
-        # Now we need to handle what happens when we shrink the window. 
-        # If we have two fruit types we need to shrink. 
-        while len(fruitFrequency) > 2:
-            # When we do shrink we need to A) remove occurence of fruit B) remove fruit from map completely if necessary.
-            leftFruit = fruits[windowStart]
-            fruitFrequency[leftFruit] -= 1 
-            if fruitFrequency[leftFruit] == 0:
-                del fruitFrequency[leftFruit]
-            # Move our window start (after we shrink window). 
-            windowStart += 1 
-        # Determine a new max length. 
-        maxLength = max(maxLength, windowEnd - windowStart + 1)
+            # Now we need to handle what happens when we shrink the window. 
+            # If we have two fruit types we need to shrink. 
+            while len(fruitFrequency) > 2:
+                # When we do shrink we need to A) remove occurence of fruit B) remove fruit from map completely if necessary.
+                leftFruit = fruits[windowStart]
+                fruitFrequency[leftFruit] -= 1 
+                if fruitFrequency[leftFruit] == 0:
+                    del fruitFrequency[leftFruit]
+                # Move our window start (after we shrink window). 
+                windowStart += 1 
+            # Determine a new max length. 
+            maxLength = max(maxLength, windowEnd - windowStart + 1)
         # Return statement. 
         return maxLength  
