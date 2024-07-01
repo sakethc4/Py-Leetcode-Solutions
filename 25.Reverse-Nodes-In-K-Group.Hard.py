6f06f1646273f6b1a48bec36e3c8d80881b6f30f
sakethc4 <saketh.c4@gmail.com>
2024-07-01 07:40:38 -0400

    sol commit.

--- 25.Reverse-Nodes-In-K-Group.Hard.py
+++ 25.Reverse-Nodes-In-K-Group.Hard.py
@@ -6,31 +6,42 @@
 class Solution: # O(N) Time & O(1) Space. 
     def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
         dummy = ListNode(0, head)
-        prevGroup = dummy 
-        # Checking how many nodes to reverse. 
+        lastNodeOfPrev = dummy 
+        # Iteratye through our input List. 
         while True: 
-            kth = self.getGroup(prevGroup, k)
-            if not kth: 
+            initialStartingNode = lastNodeOfPrev.next 
+            # Handle subList. 
+            subList = self.getSubList(lastNodeOfPrev, k)
+            if subList is None:
                 break 
-            nextGroup = kth.next 
-            # Now do reversal. 
-            current, previous = prevGroup.next, kth.next
-            while current != nextGroup:
-                tmp = current.next
-                current.next = previous
-                previous = current
-                current = tmp
+            firstNodeOfNext = subList.next 
+            # Now let's handle reversal. 
+            current, previous = lastNodeOfPrev.next, None
+            while current != firstNodeOfNext: 
+                nxt = current.next 
+                current.next = previous 
+                previous = current 
+                current = nxt
+            # Let's handle connecting sublists. 
+            # switch dummy.next pointer.
+            lastNodeOfPrev.next = subList
+            # Switch initialStarting. 
+            initialStartingNode.next = firstNodeOfNext
+            # Switch lastNodeOfnext. 
+            lastNodeOfPrev = initialStartingNode 
+        return dummy.next   
 
-            tmp = prevGroup.next
-            prevGroup.next = kth
-            prevGroup = tmp
-        return dummy.next
-
-    def getGroup(self, current, k):
-        while current and k > 0:
-            current = current.next
+    
+    # Helper method to find sublists. 
+    def getSubList(self, current, k):
+        while current and k > 0: 
+            current = current.next 
             k -= 1
         return current 
+
+         
+
+
 
 
          
