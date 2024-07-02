08e9cadb8a4bf670d8fb93adb8ddc395ea0623d1
sakethc4 <saketh.c4@gmail.com>
2024-07-02 05:58:55 -0400

    sol commit.

---
+++ 350.Intersection-Of-Two-Arrays-II.EZ.py
@@ -0,0 +1,8 @@
+class Solution: # O(N) Time & O(1) Space. 
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        result = [] 
+        for num in nums1: 
+            if num in nums2:
+                result.append(num)
+                nums2.remove(num)
+        return result  
