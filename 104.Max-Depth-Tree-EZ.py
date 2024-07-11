9eaec8040579d16904d65ec4784b5469b1575528
sakethc4 <saketh.c4@gmail.com>
2024-07-11 18:02:18 -0400

    sol commit.

---
+++ 104.Max-Depth-Tree-EZ.py
@@ -0,0 +1,16 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+# DFS approach. 
+class Solution: # O(N) Time & O(N) Space. 
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+        
+        
+         
