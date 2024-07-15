1bf8fc183aaed8535239935cc98551c97912b98e
sakethc4 <saketh.c4@gmail.com>
2024-07-15 18:20:51 -0400

    sol commit.

--- 56.Merge-Intervals-Med.py
+++ 56.Merge-Intervals-Med.py
@@ -1,12 +1,11 @@
 class Solution: # O(N) Time & O(N) Space. 
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort(key=lambda x: x[0])
         result = [intervals[0]] 
-        if len(intervals) < 2: 
-            return intervals 
-        intervals.sort(key:lambda, x: x[0])
         for interval in intervals:
-            if result[-1][1] > interval[0]:
+            if result[-1][1] >= interval[0]:
                 result[-1][1] = max(result[-1][1], interval[1])
             else:
                 result.append(interval)
         return result
+ 
